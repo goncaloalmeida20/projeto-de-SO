140064a04f8189ee226cae814690e1a053c850b4
joaobjsantos <93948049+joaobjsantos@users.noreply.github.com>
2022-03-31 16:30:49 +0100

    Retirar coisas inúteis

--- mobile_node.c
+++ mobile_node.c
@@ -1,49 +1,32 @@
 #include <stdio.h>
-#include <pthread.h>
 #include <stdlib.h>
-#include <semaphore.h>
 #include <unistd.h>
 
-#define MAX_THREADS 1000
+typedef struct request{
+	int thousand_inst;
+	int max_exec_time;
+}Request;
 
-int req_interval, thousand_inst_per_req, max_exec_time;
-pthread_mutex_t req_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-void *request(void* t){
-	int i, req_id = *((int*)t);
-	
-	pthread_mutex_lock(&req_mutex);
-	printf("Request %d sent\n", req_id);
-	for(i = 0; i < thousand_inst_per_req*1000; i++){
-	}
-	usleep(req_interval);
-	pthread_mutex_unlock(&req_mutex);
-	
-	pthread_exit(NULL);
-}
+Request r;
 
 int main(int argc, char *argv[]){
-	pthread_t threads[MAX_THREADS];
-	int i, n_requests, id[MAX_THREADS];
+	int i, n_requests, req_interval_ms;
 	
 	if(argc != 5){
-		printf("Wrong number of parameters");
+		printf("Wrong number of parameters\n");
 		exit(0);
 	}
 	
 	n_requests = atoi(argv[1]);
-	req_interval = atoi(argv[2]);
-	thousand_inst_per_req = atoi(argv[3]);
-	max_exec_time = atoi(argv[4]);
+	req_interval_ms = atoi(argv[2]);
+	r.thousand_inst = atoi(argv[3]);
+	r.max_exec_time = atoi(argv[4]);
 	
 	for(i = 0; i < n_requests; i++){
-		id[i] = i;
-		pthread_create(&threads[i], NULL, request, &id[i]); 
+		printf("Generating request %d with %d instructions and with max execution time %d s\n", i, r.thousand_inst*1000, r.max_exec_time);
+		printf("Sleeping %d milliseconds...\n", req_interval_ms);
+		usleep(req_interval_ms*1000);
 	}
-	
-	for(i = 0; i < n_requests; i++) pthread_join(threads[i], NULL);
-	
-	pthread_mutex_destroy(&req_mutex);
 	
 	exit(0);
 }
